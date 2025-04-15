aeebe259e7222a9a573a6a7f1f973ec5097547e5
jash1507patel <jashpatelprec1507@gmail.com>
2025-04-15 18:31:50 +0530

    changing in login and signup

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,16 +1,17 @@
 import React, { useContext, useEffect, useState } from "react";
-import {useNavigate} from "react-router"
+import { useNavigate } from "react-router";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
 import { AuthContext } from "../context/AuthContext";
+import "./Login.css";
+import { Card, Typography } from "@mui/material";
 
 const adminuser = { email: "admin@gmail.com", password: "admin123" };
 
 function Login() {
-
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const { login, role } = useContext(AuthContext);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -18,50 +19,62 @@ function Login() {
   function handleSubmit(e) {
     e.preventDefault();
     if (email === adminuser.email && password === adminuser.password) {
-      login("admin" , email , password);
+      login("admin", email, password);
     } else {
-      login("user" , email , password);
+      login("user", email, password);
     }
     setEmail("");
     setPassword("");
   }
 
   useEffect(() => {
-    if(role){
-      navigate(role === "admin" ? "/admin" : "/user")
+    if (role) {
+      navigate(role === "admin" ? "/admin" : "/user");
     }
-  })
+  });
 
   return (
-      <Box
-        component="form"
-        sx={{ "& > :not(style)": { m: 1, width: "52ch" } }}
-        noValidate
-        autoComplete="off"
+    <div className="mainContainer">
+      <Card
+        sx={{
+          padding: "20px",
+          boxShadow: "5px 5px 25px  gray",
+          borderRadius: "15px",
+        }}
+      >
+        <Box
+          component="form"
+          sx={{ "& > :not(style)": { m: 1, width: "400px" } }}
+          noValidate
+          autoComplete="off"
         >
-        <h1>login</h1>
-        <TextField
-          id="name"
-          label="Email"
-          variant="outlined"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <br />
-        <TextField
-          id="password"
-          label="Password"
-          variant="outlined"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <br />
-        <Stack spacing={2} direction="row">
-          <Button variant="contained" onClick={handleSubmit}>
-            Log in
-          </Button>
-        </Stack>
-      </Box>
+          <div className="heading">
+          <Typography variant="h4" sx={{fontWeight:"700"}}>LogIn</Typography>
+          </div>
+          <TextField
+            id="name"
+            label="Email"
+            variant="outlined"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <br />
+          <TextField
+            id="password"
+            label="Password"
+            variant="outlined"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <br />
+          <Stack spacing={2} direction="row" sx={{alignItems:"center" , justifyContent : "center"}}>
+            <Button variant="contained" onClick={handleSubmit}>
+              Log in
+            </Button>
+          </Stack>
+        </Box>
+      </Card>
+    </div>
   );
 }
 
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,60 +1,119 @@
 import React, { useContext, useState } from "react";
-import {useNavigate} from "react-router"
+import { useNavigate } from "react-router";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
 import { AuthContext } from "../context/AuthContext";
+import { Card, Typography } from "@mui/material";
+import "./Signup.css"
 
 function Signup() {
-  const navigate = useNavigate()
-  const {signup} = useContext(AuthContext)
-  const [signupUser,setSignupUser] = useState({
-    name : "",
-    age : "",
-    email : "",
-    address : "",
-    password : "",
-  })
+  const navigate = useNavigate();
+  const { signup } = useContext(AuthContext);
+  const [signupUser, setSignupUser] = useState({
+    name: "",
+    age: "",
+    email: "",
+    address: "",
+    password: "",
+  });
 
-  function handleSubmit(e){
+  function handleSubmit(e) {
     e.preventDefault();
-    signup({...signupUser , id : Date.now() })
+    signup({ ...signupUser, id: Date.now() });
     setSignupUser({
-      name : "",
-      age : "",
-      email : "",
-      address : "",
-      password : "",
-    })
-    navigate("/login")
+      name: "",
+      age: "",
+      email: "",
+      address: "",
+      password: "",
+    });
+    navigate("/login");
   }
 
   return (
-    <div>
-      <h1>SignUP</h1>
-      <Box
-        component="form"
-        sx={{ "& > :not(style)": { m: 1, width: "25ch" } }}
-        noValidate
-        autoComplete="off"
+    <div className="mainContainer">
+      <Card
+        sx={{
+          padding: "20px",
+          boxShadow: "5px 5px 25px  gray",
+          borderRadius: "15px",
+        }}
       >
-        <TextField id="name" label="Name" variant="outlined" value={signupUser.name} onChange={(e) => setSignupUser({...signupUser,name : e.target.value})}/>
-        <TextField id="age" label="Age" type="number" value={signupUser.age} onChange={(e) => setSignupUser({...signupUser,age : e.target.value})}/>
-        <br />
-        <TextField id="email" label="Email" variant="outlined" value={signupUser.email} onChange={(e) => setSignupUser({...signupUser,email : e.target.value})}/>
-        <TextField id="address" label="Address" variant="outlined" value={signupUser.address} onChange={(e) => setSignupUser({...signupUser,address : e.target.value})}/>
-        <br />
+          <div className="heading">
+            <Typography variant="h4" sx={{ fontWeight: "700" }}>
+              SignUP
+            </Typography>
+          </div>
         <Box
-          sx={{ "& > :not(style)": {  width: "52ch" } }}
+          component="form"
+          sx={{ "& > :not(style)": { m: 1, width: "200px" } }}
+          noValidate
+          autoComplete="off"
         >
-          <TextField id="password" label="Password" variant="outlined" value={signupUser.password} onChange={(e) => setSignupUser({...signupUser,password : e.target.value})}/>
+          <TextField
+            id="name"
+            label="Name"
+            variant="outlined"
+            value={signupUser.name}
+            onChange={(e) =>
+              setSignupUser({ ...signupUser, name: e.target.value })
+            }
+            sx={{}}
+          />
+          <TextField
+            id="age"
+            label="Age"
+            type="number"
+            value={signupUser.age}
+            onChange={(e) =>
+              setSignupUser({ ...signupUser, age: e.target.value })
+            }
+          />
+          <br />
+          <TextField
+            id="email"
+            label="Email"
+            variant="outlined"
+            value={signupUser.email}
+            onChange={(e) =>
+              setSignupUser({ ...signupUser, email: e.target.value })
+            }
+          />
+          <TextField
+            id="address"
+            label="Address"
+            variant="outlined"
+            value={signupUser.address}
+            onChange={(e) =>
+              setSignupUser({ ...signupUser, address: e.target.value })
+            }
+          />
+          <br />
+          <Box sx={{ "& > :not(style)": { width: "415px" } }}>
+            <TextField
+              id="password"
+              label="Password"
+              variant="outlined"
+              value={signupUser.password}
+              onChange={(e) =>
+                setSignupUser({ ...signupUser, password: e.target.value })
+              }
+            />
+          </Box>
+          <br />
         </Box>
-        <br />
-        <Stack spacing={2} direction="row">
-          <Button variant="contained" onClick={handleSubmit}>Submit</Button>
-        </Stack>
-      </Box>
+          <Stack
+            spacing={2}
+            direction="row"
+            sx={{ alignItems: "center", justifyContent: "center" }}
+          >
+            <Button variant="contained" onClick={handleSubmit}>
+              Submit
+            </Button>
+          </Stack>
+      </Card>
     </div>
   );
 }
